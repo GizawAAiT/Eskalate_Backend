536bc22f13cd1831b1c281e7bf7cfe754377dce4
Gizaw Dagne <gizawaait@gmail.com>
2023-04-27 15:29:30 +0300

    Create Application Project For Clean Architecture

---
+++ HR.LeaveManagement/src/Core/HR.LeaveManagement.Application/Percistence/Contracts/IGenericRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Contracts
+{
+    public interface IGenericRepository<T>  where T : class
+    {
+        Task<T> Get(int id);
+        Task<IReadOnlyList<T>> GetAll();
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
+    }
+}
