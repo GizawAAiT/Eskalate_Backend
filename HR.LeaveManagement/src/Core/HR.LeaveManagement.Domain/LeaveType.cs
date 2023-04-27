426483ffa48bca6f222764577000746b154b1a51
Gizaw Dagne <gizawaait@gmail.com>
2023-04-27 14:45:30 +0300

    Create Domain Project

---
+++ HR.LeaveManagement/src/Core/HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveType : BaseDomainEntity
+    {
+        // public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        // public DateTime DateCreated { get; set; }
+    }
+}
