55dd273676136879c950da62cd8a5cb5d6436f9a
Gizaw Dagne <gizawaait@gmail.com>
2023-04-28 13:50:59 +0300

    Do Auto Mapper

--- HR.LeaveManagement/src/Core/HR.LeaveManagement.Domain/LeaveAllocation.cs
+++ HR.LeaveManagement/src/Core/HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -9,7 +9,7 @@ namespace HR.LeaveManagement.Domain
         // public int Id { get; set; }
         public int NumberOfDays { get; set; }
         // public DateTime DateCreated { get; set; }
-        public LeaveType LeaveType { get; set; }
+        public LeaveType? LeaveType { get; set; }
         public int LeaveTypeId { get; set; }
         public int Period { get; set; }
     }
